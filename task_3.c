b0edff88dd4fdff19253ff86762fd8584f12f1e2
Runchao Han <408063141@qq.com>
2018-03-05 16:16:25 +0000

    1

--- task_3.c
+++ task_3.c
@@ -25,6 +25,15 @@ void init_matrixes(){
 		}
 }
 
+void print_matrix(int matrix[N][N], int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void matrix_multiply_basic()
 {
 	int i,j,k;
@@ -34,48 +43,62 @@ void matrix_multiply_basic()
 				matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
 }
 
+void transpose(int m[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i; j < N; j++)
+        {
+            int temp = m[i][j];
+            m[i][j] = m[j][i];
+            m[j][i] = temp;
+        }
+    }
 
+}
 
 void matrix_multiply_opt()
 {
-	
+
+    transpose(matrixB);
+
 	int i,j,k;
 	for (i = 0 ; i < N ; i+=1)
 		for(j = 0 ; j < N ; j +=1)
 			for(k = 0 ; k < N ; k +=1)
-				matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+				matrixC[i][j] += matrixA[i][k] * matrixB[j][k];
 }
 
 int main()
 {
 	init_matrixes();/* Initializes matrix elements */
 	int perf, perf_cache_miss, perf_cache_access;
-	
+
 	// init performance events count
 	//for description of events write "man perf_event_open" in console
-	//perf = setup_perf(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES); 
+	//perf = setup_perf(PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES);
 	perf_cache_miss = setup_perf_cache(PERF_COUNT_HW_CACHE_LL, PERF_COUNT_HW_CACHE_OP_READ, PERF_COUNT_HW_CACHE_RESULT_MISS);
 	perf_cache_access = setup_perf_cache(PERF_COUNT_HW_CACHE_LL, PERF_COUNT_HW_CACHE_OP_READ, PERF_COUNT_HW_CACHE_RESULT_ACCESS);
-	
+
 	clock_t begin_time = clock();
-	
+
 	start_perf(perf_cache_miss);		// start performance events count
 	start_perf(perf_cache_access);		// start performance events count
-	
+
 	matrix_multiply_basic();
-	
+
 	end_perf(perf_cache_miss);			// stop performance events count
 	end_perf(perf_cache_access);			// stop performance events count
-	
+
 	clock_t end_time = clock();
-	
+
+    print_matrix(matrixC, 10);
+
 	printf("Execution took %f seconds.\n", (end_time-begin_time)/1000000.0);
-	
+
 	print_perf(perf_cache_miss);		// print performance events count
 	print_perf(perf_cache_access);		// print performance events count
-	
+
 	return 0;
 
 }
-
-
